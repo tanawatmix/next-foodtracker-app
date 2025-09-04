a88b631d94d93e71895535231ddf35a2d57365ca
MM <oodollaroo.mix47@gmail.com>
2025-09-04 13:37:21 +0700

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,37 @@
+import type { Metadata } from "next";
+import { Prompt } from "next/font/google";
+import "./globals.css";
+
+const prompt = Prompt({
+  subsets: ["thai", "latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
+
+export const metadata: Metadata = {
+  title: "Food Tracker App V.1.0",
+  description: "Food Tracker for everybody",
+  keywords: ["Food", "Tracker", "App", "อาหาร", "ติดตาม"],
+  icons: {
+    icon: "/next.svg",
+  },
+  authors: [{
+    name: "Tarasato",
+    url: "https://github.com/Tarasato"
+  }],
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${prompt.className}`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="">
+      <h1>FoodTracker by TANAWAT</h1>
+      <h3>ยินดีต้อนรับ</h3>
+    </div>
+  );
+}
